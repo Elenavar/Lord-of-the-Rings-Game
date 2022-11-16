0dc1e14adf2496449cf328949b75aa66abcc306e
Elena <elenavargasm9@gmail.com>
2022-11-16 11:56:24 +0100

    add style

--- js/main.js
+++ js/main.js
@@ -1,64 +1,85 @@
 'use strict';
 
 //Declaración de variables
-let bien = 0;
-let mal = 0;
+let good = 0;
+let evil = 0;
 let i = 0;
+const finalResult = document.querySelector('.js-final-result');
 const select = document.querySelector('.js-select');
 const result = document.querySelector('.js-result');
 const button = document.querySelector('.js-button');
+const round = document.querySelector('.js-round');
 const restartButton = document.querySelector('.js-restart-button');
 const gamer = document.querySelector('.js-gamer');
 const pc = document.querySelector('.js-pc');
 
-const races = [ 2 /*sureños*/, 2 /*orcos*/, 2 /*goblins*/, 3 /*huargos*/, 5 /*trolls*/];
-
+const races = [
+  2 /*sureños*/, 2 /*orcos*/, 2 /*goblins*/, 3 /*huargos*/, 5 /*trolls*/,
+];
 
 //Funciones
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function counter(){
-  if (i <= 10){
-    startGame(getRandomNumber(6));
-    console.log(`vuelta ${i}`);
-    i++;
-  }else{
-    button.classList.add('hide');
-    restartButton.classList.remove('hide');
+function gameOver() {
+  if (good > evil) {
+    finalResult.innerHTML = '¡Ha ganado el ejército del bien!';
+  } else {
+    finalResult.innerHTML = 'Oh no.. Ha ganado el ejército del mal.';
   }
 }
-function startGame(number){
-    console.log(number);
-    const selectValue = select.value;
-    if (races[number]< selectValue) {
-        result.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
-        bien++;
-        gamer.innerHTML = `Jugador: ${bien}`;
-    } else if (races[number]> selectValue) {
-        result.innerHTML = "Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
-        mal++;
-        pc.innerHTML = `Computadora ${mal}`;
-    } else {
-      result.innerHTML = "Empate";
-    }
+
+function counter() {
+  if (i < 10) {
+    startGame();
+    round.innerHTML = `Ronda: ${i + 1}`;
+    i++;
+  } else {
+    button.classList.add('hide');
+    restartButton.classList.remove('hide');
+    gameOver();
+  }
 }
 
+function compare(number, selectValue) {
+  if (races[number] < selectValue) {
+    result.innerHTML = 'Ha ganado el Ejército del Bien! Enhorabuena.';
+    good++;
+    gamer.innerHTML = `Jugador: ${good}`;
+  } else if (races[number] > selectValue) {
+    result.innerHTML = 'Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
+    evil++;
+    pc.innerHTML = `Computadora: ${evil}`;
+  } else {
+    result.innerHTML = 'Empate';
+  }
+}
+
+function startGame() {
+  const selectValue = select.value;
+  const number = getRandomNumber(6);
+  compare(number, selectValue);
+}
 
 function handleClick(event) {
   event.preventDefault();
   counter();
 }
 
-function restart(){
+function restart() {
   i = 0;
-  bien = 0;
-  mal = 0;
+  good = 0;
+  evil = 0;
+  round.innerHTML = '';
+  result.innerHTML = '';
+  pc.innerHTML = `Computadora: ${evil}`;
+  gamer.innerHTML = `Jugador: ${good}`;
+  finalResult.innerHTML = '';
   button.classList.remove('hide');
   restartButton.classList.add('hide');
 }
-function handleRestart(event){
+function handleRestart(event) {
   event.preventDefault();
   restart();
 }
